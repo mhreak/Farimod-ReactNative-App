391c8e08669353cd63e6dce6af570d2fde470173
Yeganeh <95122152+yeganeh1@users.noreply.github.com>
2025-08-03 12:15:23 +0330

    sth fixed

--- app/Navigators.tsx
+++ app/Navigators.tsx
@@ -17,12 +17,13 @@ import UserProfileScreen from "./screens/UserProfileScreen";
 import SubscriptionScreen from "./screens/SubscriptionScreen";
 import AddNewPostScreen from "./screens/AddNewPostScreen";
 import PortfolioListScreen from "./screens/PortfolioListScreen";
-
-
+ 
+import CareerScreen from "./screens/intro/CareerScreen"; 
+import WelcomeIntroScreen from "./screens/intro/WelcomeIntroScreen";
 import WhyFrimodScreen from "./screens/intro/WhyFrimodScreen";
 import EventsScreen from "./screens/intro/EventsScreen";
-import CareerScreen from "./screens/intro/CareerScreen";
-import WelcomeIntroScreen from "./screens/intro/WelcomeIntroScreen";
+
+
 
 import styles from "./config/styles";
 import MagDetailesScreen from "./screens/MagDetailesScreen";
